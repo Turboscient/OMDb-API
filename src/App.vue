979d75627b24b03c78a28c948306986abef0e78e
turboscient <gondes1@mcmaster.ca>
2021-05-03 00:32:31 -0400

    UI changes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300&display=swap" rel="stylesheet"> 
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -18,11 +19,11 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'IBM Plex Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 2vh;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,124 +1,142 @@
 <template>
-  <div class="hello">
-    <div>
-      <h1>OMDb API Movie Search</h1>
-      <div style="width: 20vw; float: left; margin-left: 5vw;">
-        <b-card       
-          tag="article"
-          style="max-width: 20rem;"
-          class="mb-2"
-        >
-          <h2><b>Movie info</b></h2>
-          <h3>Title: {{ JSON.parse(jsonRes).Title }}</h3>
-          <h3>Released: {{ JSON.parse(jsonRes).Released }}</h3>
-          <h3 v-for="rating in JSON.parse(jsonRes).Ratings" :key="rating.Source">
-              {{ rating.Source }}: {{ rating.Value }}
-          </h3>
-          <div style="margin-bottom: 2vw;"></div>
-          
-            <b-button href="#" 
-                      v-if="notInMovieList(JSON.parse(jsonRes).imdbID)"
-                      @click="nominationProcess((JSON.parse(jsonRes)))" 
-                      variant="outline-primary">Nominate
-            </b-button>      
-           
-        </b-card>
-      </div>
-
-      <div style="float: left;">
-        <b-card
-          title="The Shoppies"
-          img-src="https://images.pexels.com/photos/3137890/pexels-photo-3137890.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-          img-alt="Movies"
-          img-top
-          tag="article"
-          style="max-width: 50vw;"
-          class="mb-2"
-        >        
-            <div style="width: 20vw; margin-left: 15vw">
-              <b-form-input v-model="text" placeholder="Search for a movie"></b-form-input>            
-            </div>        
-        </b-card>
-      </div>
-
-      <div style="width: 20vw; float: left; margin-left: 2vw;">
-        <b-card       
-          tag="article"
-          style="max-width: 20rem;"
-          class="mb-2"
-        >
-          <h2><b>Nominated</b></h2>
-          <h3 v-for="nomination in nominated" :key="nomination.imdbID">                 
-            {{ nomination.Title }}
-            <b-button href="#"       
-              @click="removeMovie(nomination.imdbID)"            
-              variant="dark">Remove
-            </b-button>                
-          </h3>
-        </b-card>
-      </div>
-    </div>    
-
-    <div>
-
-      <b-modal id="modal-1" title="OMDb Notification">
+  <span>
+    <b-container fluid>
+      <b-row>
+        <b-col><h1 class="text-center">OMDb API Movie Search</h1></b-col>
+      </b-row>
+      <b-row style="background-color: #e0e0eb">
+        <b-col cols="3">
+          <b-card tag="article" class="mb-4 shadow-sm">
+            <h2><b>Movie info</b></h2>
+          </b-card>
+          <b-card v-if="json.Title" class="text-left">
+            <h3><strong>Title</strong></h3>
+            <h4>{{ json.Title }}</h4>
+            <h3><strong>Released</strong></h3>
+            <h4>{{ json.Released }}</h4>
+            <div v-for="rating in json.Ratings" :key="rating.Source">
+              <h3>
+                <strong>{{ rating.Source === 'Internet Movie Database' ? 'IMDb' : rating.Source }}</strong>
+              </h3>
+              <h4>{{ rating.Value }}</h4>
+            </div>
+            <div style="margin-bottom: 2vh"></div>            
+          </b-card>
+        </b-col>
+        <b-col cols="6">
+          <b-card
+            title="The Shoppies"
+            img-src='https://images.pexels.com/photos/3137890/pexels-photo-3137890.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            img-alt="Movies"
+            img-top
+            tag="article"
+            style="max-width: 50vw"
+            class="mb-2 shadow"
+          >
+            <b-row class="justify-content-center">
+              <b-col cols="9">
+                <b-form-input
+                  v-model="text"
+                  placeholder="Search for a movie"
+                ></b-form-input>
+              </b-col>
+              <b-col cols="3">              
+                <b-button
+                  href="#"
+                  v-if="notInMovieList(json.imdbID)"
+                  @click="nominationProcess(json)"
+                  variant="info"
+                  class="btn-block"                  
+                  >Nominate
+                </b-button>
+              </b-col>
+            </b-row>
+          </b-card>
+        </b-col>
+        <b-col cols="3">
+          <b-card tag="article" class="mb-4 shadow-sm">
+            <h2><b>Nominated</b></h2>
+          </b-card>
+          <b-card
+            v-for="nomination in nominated"
+            :key="nomination.imdbID"
+            class="mb-2 shadow"
+          >
+            <h3>
+              {{ nomination.Title }}
+            </h3>
+            <b-button
+              href="#"
+              @click="removeMovie(nomination.imdbID)"             
+              variant="warning"             
+              ><span style="color: black">Remove</span>
+            </b-button>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+    <b-modal id="modal-1" title="OMDb Notification">
         <p class="my-4">You've added five movies!</p>
-      </b-modal>
-      
-    </div>
-  </div>
+    </b-modal>
+  </span>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',  
-  data: () => ({
-    jsonRes: null,
+  name: "HelloWorld",
+  data: () => ({    
+    json: null,
     poster: null,
-    text: '',
-    nominated: []
+    text: "",
+    nominated: [],
   }),
   methods: {
     async fetchText(title) {
-      let res = await fetch('https://www.omdbapi.com/?apikey=a72d8c19&t=' + title)
-      let pos = await fetch('https://img.omdbapi.com/?apikey=a72d8c19&t=' + title)
-      let data = await res.text()          
-      this.jsonRes = data    
-      this.poster = pos    
+      let res = await fetch(
+        "https://www.omdbapi.com/?apikey=4a20833c&t=" + title
+      );
+      let pos = await fetch(
+        "https://img.omdbapi.com/?apikey=4a20833c&t=" + title
+      );
+      let data = await res.text();      
+      this.poster = pos;
+      console.log(this.poster)
+      this.json = JSON.parse(data);      
     },
     removeMovie(id) {
-      this.nominated = this.nominated.filter(nomination => nomination.imdbID !== id) 
+      this.nominated = this.nominated.filter(
+        (nomination) => nomination.imdbID !== id
+      );
     },
-    notInMovieList(id) {
-      const newArr = [...this.nominated]      
-      return newArr.filter(nomination => nomination.imdbID == id).length == 0
+    notInMovieList(id) {      
+      return this.nominated.filter((nomination) => nomination.imdbID == id).length === 0;
     },
-    nominationProcess(elem) {  
-      console.log(elem)    
-      console.log(elem.Response)
+    nominationProcess(elem) {
+      console.log(elem);
+      console.log(elem.Response);
       if (elem.Response === "True") {
-        this.nominated.push(elem)
+        this.nominated.push(elem);
         if (this.nominated.length == 5) {
-          this.$bvModal.show('modal-1') 
+          this.$bvModal.show("modal-1");
         }
       }
-    }
+    },
   },
   created() {
-    this.fetchText('')
+    this.fetchText("");
   },
   watch: {
     text: function () {
-      this.fetchText(this.text)
-    }
-  }
-}
+      this.fetchText(this.text);
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+  margin-top: 0.5rem;
 }
 ul {
   list-style-type: none;
@@ -129,6 +147,6 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: white;
 }
 </style>
